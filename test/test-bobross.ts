9526ffbd5254156f648d130a9aca681abdef5f45
Ucaly <yukarim777@gmail.com>
2020-11-29 15:02:55 -0800

    Added performance metricx

--- test/test-bobross.ts
+++ test/test-bobross.ts
@@ -8,40 +8,39 @@ const bobRossUrl = 'https://www.bobrosslipsum.com';
 
 describe('Test bobrosslipsum.com ', () => {
     before(async () => {
-        console.log('log1');
         if (process.env.BASEURL) {
             baseUrl =  process.env.BASEURL;
         } else {
             baseUrl = bobRossUrl;
         }
-        console.log('log2');
+        console.time('load');
         // browser = await puppeteer.launch({executablePath: 'google-chrome-unstable', headless: true, slowMo: 10, devtools: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']});
         browser = await puppeteer.launch({headless: true, slowMo: 10, devtools: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']});
-        console.log('log3');
+
         page = await browser.newPage();
-        console.log('log4');
+
         await page.setViewport({width: 1200, height: 800});
-        console.log('log5');
+
         await page.setRequestInterception(true);
-        console.log('log6');
+
         page.on('request', (request) => {
             request.continue();
         })
-        console.log('log7');
+
         page.on('response', async (response) => {
             if(response.url().includes(bobRossUrl)) {
-                console.log(`RES: ${await response.url()}`);
-                console.log(`RES: ${await response.status()}`);
+                // console.log(`RES: ${await response.url()}`);
+                // console.log(`RES: ${await response.status()}`);
             } else {
                 if (response.status() >= 400) {
-                    console.log(`RES: ${await response.url()}`);
-                    console.log(`RES: ${await response.status()}`); 
+                    // console.log(`RES: ${await response.url()}`);
+                    // console.log(`RES: ${await response.status()}`); 
                 }
             }
         })
-        console.log('log8');
+
         await page.goto(baseUrl, {waitUntil: 'networkidle2'});
-        console.log('log9');
+        console.timeEnd('load');
     });
 
     it('Verify page url ', async () => {
@@ -63,6 +62,16 @@ describe('Test bobrosslipsum.com ', () => {
                 throw 'Timeout error';
             }
         }
+    })
+
+    it('Performance metrics', async () => {
+        const performanceTimingJson = await page.evaluate(() => JSON.stringify(window.performance.timing))
+        const performanceTiming = JSON.parse(performanceTimingJson)
+      
+        // console.log(performanceTiming)
+      
+        const startToInteractive = performanceTiming.domInteractive - performanceTiming.navigationStart
+        console.log(`Navigation start to DOM interactive: ${startToInteractive}ms`)
     })
 
     after(async () => {
